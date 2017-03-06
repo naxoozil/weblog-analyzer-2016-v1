e0685f56770289c46dca8cf1c708e296f7b5026a
naxoozil <nachoviuda@gmail.com>
2017-03-06 13:25:23 +0100

    0543.b primercommit

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -64,7 +64,7 @@ public class AnalizadorAccesosAServidor
     
     public String paginaWebMasSolicitada() 
     {
-        String paginaADevolver = "";
+        String paginaADevolver = null;
         Map<String, Integer> nombreMap = new HashMap<String,Integer>();
         if (!accesos.isEmpty()) {
             for (Acceso accesoActual : accesos){
